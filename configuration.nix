7f044000719d7115f4d2f28d0f0966cd0fc497e4
Jerry Quintero <jerry@nixos.vbox>
2025-12-10 23:02:37 -0600

    Using modern GNOME config and added comment for KDE.

--- configuration.nix
+++ configuration.nix
@@ -47,11 +47,13 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.displayManager.plasma.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
 
   # Configure keymap in X11
