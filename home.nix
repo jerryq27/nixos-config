b63a82c172ac1253f0e5d3a979f0e0dd3ed21459
Jerry Quintero <jerry@nixos.vbox>
2025-12-29 17:43:44 -0600

    Added bash aliases and vscode.

--- home.nix
+++ home.nix
@@ -10,6 +10,25 @@
         # htop
         # fastfetch
     ];
+    
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            ll = "ls -l";
+            la = "ls -a";
+            lal = "ls -al";
+            vim = "nvim";
+            nix-check = "nix flake check";
+            nix-update = "sudo nixos-rebuild switch --flake .#vbox";
+            nix-clean = "nix-collect-garbage -d";
+        };
+        bashrcExtra = ''
+            mkcd() {
+                mkdir $1
+                cd $1
+            }
+        '';
+    };
 
     # Basic configuration for programs
     programs.git = {
@@ -27,6 +46,8 @@
             XDG_PROJECTS_DIR = "${config.home.homeDirectory}/Projects";
         };
     };
+    
+    programs.vscode.enable = true;
     
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
