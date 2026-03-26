61145938f6c41a873925faee498dce0693f452cc
Jerry Quintero <jerry@nixos.vbox>
2026-03-26 12:22:55 -0500

    Added additional packages from laptop.

--- home/desktop.nix
+++ home/desktop.nix
@@ -10,17 +10,26 @@
       XDG_PROJECTS_DIR = "/home/${config.home.username}/Projects";
     };
   };
-  
+
+  programs.anki.enable = true;
   programs.vscode.enable = true;
   programs.onlyoffice.enable = true;
   programs.obs-studio.enable = true;
   programs.obsidian.enable = true;
   programs.zed-editor.enable = true;
+  programs.spotify-player.enable = true;
 
   # Packages without a Home Manager module
   home.packages = with pkgs; [
+    # beekeeper-studio
+    filezilla
     gimp
+    gnome-tweaks
     godot
     htop
+    inkscape
+    postman
+    qbittorrent
+    steam
   ];
 }
